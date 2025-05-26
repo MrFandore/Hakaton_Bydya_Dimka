d9ce1c490814490ff92152696864c91e06adf7f8
MrFandore <99207909+MrFandore@users.noreply.github.com>
2025-05-26 22:24:10 +0300

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-# import torch
-# print("PyTorch version:", torch.__version__)
-# print("CUDA available:", torch.cuda.is_available())
-
-
-import torch
-print(f"PyTorch version: {torch.__version__}")
-print(f"CUDA available: {torch.cuda.is_available()}")
-print(f"GPU name: {torch.cuda.get_device_name(0)}")  # Если CUDA доступна
