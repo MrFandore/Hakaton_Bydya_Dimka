c24b2dc7b2950dcc06b5405b80345fdbc160f90a
MrFandore <99207909+MrFandore@users.noreply.github.com>
2025-05-26 22:22:22 +0300

    Update urls.py

--- voice_restore/urls.py
+++ voice_restore/urls.py
@@ -5,9 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),  # Подключаем маршруты из приложения core
+    path('', include('core.urls')),  
 ]
 
-# Только для режима разработки! Добавляем медиафайлы
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
