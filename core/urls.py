e839224ed381f7d7f57c0d5c22d921395172580b
MrFandore <99207909+MrFandore@users.noreply.github.com>
2025-05-26 22:27:03 +0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.upload_audio, name='upload_audio'),  # Главная страница
+    path('', views.upload_audio, name='upload_audio'),
 ]
